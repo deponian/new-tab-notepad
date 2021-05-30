b673c454781829a0abadabca99f9064d7bdc7201
Rufus Deponian <deponian@pm.me>
2021-05-30 17:40:23 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# New Tab Notepad for Chromium-based browsers
+
+## Features
+
+* Persistent notepad on the "New Tab" page using Chrome's storage API.
+
+## Credits
+
+* Matt Sweeney and his [repository](https://github.com/sweenzor/new-tab-notepad) where I took most of the code.
+
+## Screenshot
+
+![](https://raw.githubusercontent.com/deponian/new-tab-notepad/master/screenshot.png)
---
+++ new-tab.js
@@ -0,0 +1,28 @@
+(function() {
+
+  function _makeDelayed() {
+    var timer = 0;
+    return function(callback, ms) {
+      clearTimeout(timer);
+      timer = setTimeout(callback, ms);
+    };
+  }
+
+  function bindNoteHandlers() {
+    var elem = document.getElementById('note-text'),
+        saveHandler = _makeDelayed();
+    function save() {
+      chrome.storage.sync.set({'noteText': elem.value});
+    }
+    // Throttle save so that it only occurs after 500 millisecond without a keypress.
+    elem.addEventListener('keydown', function() {
+      saveHandler(save, 500);
+    });
+    elem.addEventListener('blur', save);
+    chrome.storage.sync.get('noteText', function(data) {
+      elem.value = data.noteText ? data.noteText : '';
+    });
+  }
+
+  bindNoteHandlers();
+})();
