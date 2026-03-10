37b781ae3da07525ea51b1db85b4753a0b1e0903
emptysad <157696806+emptysad@users.noreply.github.com>
2026-03-10 21:41:35 +0500

    fix 🤙

--- new-tab.js
+++ new-tab.js
@@ -1,15 +1,13 @@
-
-  function bindNoteHandlers() {
-    const elem = document.getElementById('note-text');
-    function save() {
-      chrome.storage.sync.set({'noteText': elem.value});
-    }
-    elem.addEventListener('keypress', save);
-    elem.addEventListener('blur', save);
-    chrome.storage.sync.get('noteText', function(data) {
-      elem.value = data.noteText ? data.noteText : '';
-    });
-
+function bindNoteHandlers() {
+  let elem = document.getElementById('note-text');
+  function save() {
+    chrome.storage.sync.set({'noteText': elem.value});
   }
+  elem.addEventListener('keyup', save);
+  elem.addEventListener('blur', save);
+  chrome.storage.sync.get('noteText', function(data) {
+    elem.value = data.noteText ? data.noteText : '';
+  });
+}
 
-  bindNoteHandlers();
+bindNoteHandlers();
