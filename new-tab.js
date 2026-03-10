59471d251bd617492114b118651ce63ff336355f
Gleb <emptysad@proton.me>
2026-03-11 03:58:10 +0500

    Deepl integration

--- new-tab.js
+++ new-tab.js
@@ -1,28 +1,95 @@
-(function() {
+const API = "https://deepl-proxy-three.vercel.app";
 
-  function _makeDelayed() {
-    var timer = 0;
-    return function(callback, ms) {
-      clearTimeout(timer);
-      timer = setTimeout(callback, ms);
-    };
+function bindNoteHandlers() {
+  let noteText = document.getElementById('note-text');
+  let note = document.getElementById('note');
+  function save() {
+    chrome.storage.sync.set({'noteText': noteText.value});
   }
+  noteText.addEventListener('keyup', save);
+  noteText.addEventListener('blur', save);
+  chrome.storage.sync.get('noteText', function(data) {
+    noteText.value = data.noteText ? data.noteText : '';
+  });
 
-  function bindNoteHandlers() {
-    var elem = document.getElementById('note-text'),
-        saveHandler = _makeDelayed();
-    function save() {
-      chrome.storage.sync.set({'noteText': elem.value});
+  document.addEventListener("keypress", async (e) => {
+    if (e.ctrlKey && e.code === "KeyL") {
+      e.preventDefault();
+      const login = prompt('Login');
+      const password = prompt('Password');
+      note.style.border = "orange 5px solid";
+
+      const body = JSON.stringify({
+          login: login,
+          password: password,
+        })
+
+      const res = await fetch(API + "/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: body
+      })
+      if (res.ok) {
+        document.cookie = "token=Bearer " + (await res.json()).token;
+        note.style.border = "none";
+      } else note.style.border = "red 5px solid"
     }
-    // Throttle save so that it only occurs after 5 millisecond without a keypress.
-    elem.addEventListener('keydown', function() {
-      saveHandler(save, 5);
-    });
-    elem.addEventListener('blur', save);
-    chrome.storage.sync.get('noteText', function(data) {
-      elem.value = data.noteText ? data.noteText : '';
-    });
-  }
 
-  bindNoteHandlers();
-})();
+    if (e.ctrlKey && e.code === "KeyE") {
+      e.preventDefault();
+      note.style.border = "orange 5px solid";
+
+      const body = JSON.stringify({
+        text: noteText.value,
+        lang: "EN"
+      })
+
+      const res = await fetch(API + "/translate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": getCookie("token")
+        },
+        body: body
+      })
+      if (res.ok) {
+        noteText.value = (await res.json()).translations[0].text;
+        note.style.border = "none";
+      } else note.style.border = "red 5px solid";
+    }
+
+    if (e.ctrlKey && e.code === "KeyR") {
+      e.preventDefault();
+      note.style.border = "orange 5px solid";
+
+      const body = JSON.stringify({
+        text: noteText.value,
+        lang: "RU"
+      })
+
+      const res = await fetch(API + "/translate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": getCookie("token")
+        },
+        body: body
+      })
+      if (res.ok) {
+        noteText.value = (await res.json()).translations[0].text;
+        note.style.border = "none";
+      } else note.style.border = "red 5px solid";
+    }
+  })
+}
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+bindNoteHandlers();
